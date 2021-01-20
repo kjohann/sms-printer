cfa21bb1883b9fb1a8aac9caaf5c38a2722c3dc1
Kristian Johannessen <kristian.johannessen@bekk.no>
2021-01-20 19:45:45 +0100

    Parser XML-fil fra input - leker med resultatet

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import parser from 'fast-xml-parser';
+import he from 'he';
 import profilePic from './noprofile.png'
 
 import MessageView from './MessageView';
 import Message from './Message';
+
+
 
 const messages = [
   { image: profilePic, content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut tortor sapien, rutrum et turpis eu, congue dapibus mi. Ut non justo blandit, sollicitudin tellus in, porttitor nunc. Integer volutpat et tellus quis malesuada.' },
@@ -33,12 +37,44 @@ const messages = [
   />;
 })
 
+let fileReader: FileReader;
+
+const handleFileRead = () => {
+  const content = fileReader.result;
+  const parsed = parser.parse(content as string, {
+    parseAttributeValue: true,
+    parseNodeValue: false,
+    ignoreAttributes: false,
+    parseTrueNumberOnly: true,
+    attributeNamePrefix: '',
+    attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),
+    tagValueProcessor : (val, tagName) => he.decode(val)
+  });
+
+  console.log('done', parsed);
+}
+
+const handleFileChosen = (file: Blob) => {
+  fileReader = new FileReader();
+  fileReader.onloadend = handleFileRead;
+  fileReader.readAsText(file);
+}
+
 function App() {
   return (
     <div className="App">
-      <MessageView>
+      <input
+        type="file"
+        accept=".xml"
+        onChange={e => {
+          if (e.target && e.target.files && e.target.files[0]) {
+            handleFileChosen(e.target.files[0])
+          }
+        }}
+      />
+      {/* <MessageView>
         {messages}
-      </MessageView>
+      </MessageView> */}
     </div>
   );
 }
